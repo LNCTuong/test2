31b5a35385c11bc24201b630250e8f000210dc3f
@S5230026 <52300266@student.tdtu.edu.vn>
2025-10-01 22:12:16 +0700

    thử tạo services

---
+++ forms/Form4.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using test2.services;
+namespace test2.forms
+{
+    public partial class Form4 : Form
+    {
+        private HelloService _helloService;
+
+        public Form4()
+        {
+            InitializeComponent();
+            _helloService = new HelloService();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string msg = _helloService.GetHelloMessage("Katun");
+            MessageBox.Show(msg, "Test Service", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+        }
+    }
+}
---
+++ services/HelloService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test2.services
+{
+    public class HelloService
+    {
+        public string GetHelloMessage(string name)
+        {
+            return $"Xin chào {name}, đây là message từ Service!";
+        }
+    }
+}
